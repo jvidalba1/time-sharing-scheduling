2865623517848e44c843ba6c8b37235686f98a52
Mateo Vidal <jvidalba@gmail.com>
2014-10-06 12:40:14 -0500

    first commit, generating rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeSharingScheduling::Application.config.secret_key_base = '24e4ab397ed3860f1c54fbd98b75a993e689ef1def8cba2d8f574c892c369cf1fd168a4013d26e9d8efbacae4348c8adf8016f0118d2ad30598ed32c2d21f875'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeSharingScheduling::Application.config.session_store :cookie_store, key: '_time-sharing-scheduling_session'
