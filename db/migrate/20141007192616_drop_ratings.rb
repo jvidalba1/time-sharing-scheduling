59a12f93c3ffb97a5702fd5e94a19cf9fdccfcbe
Mateo Vidal <jvidalba@gmail.com>
2014-10-07 16:08:55 -0500

    refactoring migrations and readme to install app in dev mode

--- db/migrate/20141007192616_drop_ratings.rb
+++ db/migrate/20141007192616_drop_ratings.rb
@@ -1,5 +1,12 @@
 class DropRatings < ActiveRecord::Migration
-  def change
+  def up
     drop_table :ratings
+  end
+
+  def down
+    create_table :ratings do |t|
+      t.integer :rate
+      t.timestamps
+    end
   end
 end
