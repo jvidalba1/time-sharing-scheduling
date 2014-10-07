df2b1c4d88b5abf815edd96ea9e02ea40f91e515
Mateo Vidal <jvidalba@gmail.com>
2014-10-06 21:36:49 -0500

    final endpoints for reservation and sending its emails

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,17 +1,25 @@
 class ReservationsController < ApplicationController
 
+  def confirmation
+    @user = User.find(params[:user_id])
+    @reservation = Reservation.find(params[:id])
+  end
+
   def new
     @user = User.find(params[:user_id])
     @reservation = @user.reservations.build
   end
 
   def create
+
     @user = User.find(params[:user_id])
     @reservation = @user.reservations.build(user_params)
+    @reservation.ip_address = request.remote_ip
 
     if @reservation.save
-      # UserMailer.confirmation_email(@user).deliver!
-      redirect_to root_path
+      ReservationMailer.reservation_confirmation(@reservation).deliver!
+      ReservationMailer.time_share_scheduled(@reservation).deliver!
+      redirect_to confirmation_user_reservation_path(@user.id, @reservation.id)
     else
       render :action => 'new'
     end
