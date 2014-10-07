0152e81706a5c2ec18d0e4084151db68fc2cc319
Mateo Vidal <jvidalba@gmail.com>
2014-10-07 15:37:36 -0500

    actions to rate a reservation in reservation controllers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,9 +2,24 @@ class ReservationsController < ApplicationController
 
   before_filter :email_confirmed?, only: [:new, :create]
 
+  def rating
+    @reservation = Reservation.find(params[:reservation_id])
+  end
+
+  def update_rating
+    @reservation = Reservation.find(params[:reservation_id])
+    @user = @reservation.user
+    if @reservation.update_attributes(reservation_params)
+      flash[:notice] = "Reservation rated"
+    else
+      flash[:notice] = "Reservation was not rated"
+    end
+    render email_confirmed_users_path
+  end
+
   def confirmation
-    @user = User.find(params[:user_id])
     @reservation = Reservation.find(params[:id])
+    @user = @reservation.user
   end
 
   def new
@@ -20,7 +35,7 @@ class ReservationsController < ApplicationController
     if @reservation.save
       ReservationMailer.reservation_confirmation(@reservation).deliver!
       ReservationMailer.time_share_scheduled(@reservation).deliver!
-      redirect_to confirmation_user_reservation_path(@user.id, @reservation.id)
+      redirect_to confirmation_reservation_path(@reservation.id)
     else
       render :action => 'new'
     end
@@ -39,6 +54,6 @@ class ReservationsController < ApplicationController
 
 
   def reservation_params
-    params.require(:reservation).permit(:date, :rooms, :user_id, :trip_reason, :pool, :rec_room, :on_site_doctor, :movie_theater, :time_machine)
+    params.require(:reservation).permit(:rate, :date, :rooms, :user_id, :trip_reason, :pool, :rec_room, :on_site_doctor, :movie_theater, :time_machine)
   end
 end
